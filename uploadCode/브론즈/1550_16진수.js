fc5916c529cf60279600620521f30d9a4957c4ab
nara04040 <nara04040@naver.com>
2022-09-18 20:34:54 +0900

    1550, 10430

--- uploadCode/브론즈/1550_16진수.js
+++ uploadCode/브론즈/1550_16진수.js
@@ -1,3 +1,6 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(file).toString().trim();
+
+let answer = parseInt(input, 16);
+console.log(answer);
