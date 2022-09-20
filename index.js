739b9745c3c037a4fca46c9151ff44e028e1387b
nara04040 <nara04040@naver.com>
2022-09-20 16:00:30 +0900

    10172_개

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 const fs = require("fs");
 const file = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(file).toString().trim().split("\n");
-
-// let a = `\\   /\\`;
-// let b = `)  ( ')`;
-// let c = `(  /  )`;
-// let d = `\\(__)|`;
-// console.log(a + "\n" + b + "\n" + c + "\n" + d);
-
-console.log(`\\    /\\`);
-console.log(` )  ( ')`);
-console.log(`(  /  )`);
-console.log(` \\(__)|`);
-
-// ₩₩ 두개 써줘도 \표시 가능
